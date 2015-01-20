e20d542a0aa58963720e93b6d13f0d09fd6be581
zipu888 <zipu405@126.com>
2015-01-20 18:07:50 +0800

    try template api

--- src/main/java/com/xiaohao/mongodb/sample/persist/mongodb/MyPersonRepositoryImpl.java
+++ src/main/java/com/xiaohao/mongodb/sample/persist/mongodb/MyPersonRepositoryImpl.java
@@ -1,9 +1,16 @@
 package com.xiaohao.mongodb.sample.persist.mongodb;
 
 import com.xiaohao.mongodb.sample.domain.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by xiaohao on 2015/1/20.
@@ -11,11 +18,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MyPersonRepositoryImpl implements MyPersonRepository {
 
+    private final static Logger logger = LoggerFactory.getLogger(MyPersonRepositoryImpl.class);
+
+
     @Autowired
     MongoTemplate template;
 
     @Override
     public void savePerson(Person person) {
         template.save(person);
+
+        List<Person> ps =template.find(new Query(Criteria.where("").is("小郝")),Person.class);
+
+        logger.debug("名字是小郝的Person:"+ps.size());
+
     }
 }
