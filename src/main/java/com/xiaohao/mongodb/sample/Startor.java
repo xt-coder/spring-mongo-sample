70052809fef62fc5761ce8d7701bda28ce68de12
zipu888 <285764190@qq.com>
2015-12-11 14:49:02 +0800

    add gridFsTemplate opt

--- src/main/java/com/xiaohao/mongodb/sample/Startor.java
+++ src/main/java/com/xiaohao/mongodb/sample/Startor.java
@@ -1,12 +1,21 @@
 package com.xiaohao.mongodb.sample;
 
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSFile;
 import com.xiaohao.mongodb.sample.domain.Person;
 import com.xiaohao.mongodb.sample.persist.mongodb.MyPersonRepository;
 import com.xiaohao.mongodb.sample.persist.mongodb.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.gridfs.GridFsCriteria.whereFilename;
 
 /**
  * Created by xiaohao on 2015/1/20.
@@ -20,6 +29,9 @@ public class Startor {
     @Autowired
     MyPersonRepository myPersonRepository;
 
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
     @PostConstruct
     public void test(){
         Person p = new Person();
@@ -27,5 +39,28 @@ public class Startor {
         p.setName("小郝");
         myPersonRepository.savePerson(p);
         personRepository.save(p);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("=====================");
+
+        GridFSFile gridFSFile =gridFsTemplate.store(this.getClass().getClassLoader().getResourceAsStream("log4j.properties"), "testtt.txt");
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println("=====================");
+        System.out.println(gridFSFile.getFilename());
+
+        System.out.println();
+        System.out.println();
+        System.out.println("=====================");
+
+        List<GridFSDBFile> result =gridFsTemplate.find(query(whereFilename().is("test.txt")));
+
+        for(GridFSDBFile gff:result){
+            System.out.println(gff.getUploadDate());
+        }
+
     }
 }
